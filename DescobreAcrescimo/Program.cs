f43d7640b0b28c5898d75a497c38fa7164c77b91
thavao <othavioact@gmail.com>
2024-04-20 22:29:57 -0300

    primeiro commit

---
+++ DescobreAcrescimo/Program.cs
@@ -0,0 +1,20 @@
+﻿double valorAtual, valorNovo, valorDeAcrescimo;
+
+Console.Write("Digite o valor atual: ");
+valorAtual = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor novo: ");
+valorNovo = double.Parse(Console.ReadLine());
+
+if (valorNovo > valorAtual)
+{
+    valorDeAcrescimo = valorNovo - valorAtual;
+    Console.WriteLine("Valor de acréscimo foi: " + valorDeAcrescimo);
+}
+else
+    Console.WriteLine("Não houve acréscimo...");
+
+Console.WriteLine("Pressione Enter para encerrar...");
+
+Console.ReadLine();
+
